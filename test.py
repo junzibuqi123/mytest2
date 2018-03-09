1671ad1ba0e315e4d0f14b2b8b049100f0ed54ef
Xujialin <junzibuqi123@163.com>
2018-03-09 17:29:27 +0800

    sokect udp

--- test.py
+++ test.py
@@ -2,22 +2,13 @@ import socket
 import _thread
 import time
 import logging
-logging.basicConfig(level=logging.DEBUG)
-sk=socket.socket(socket.AF_INET,socket.SOCK_STREAM)  # 创建socket对象
-sk.bind(("127.0.0.1", 8888))
-sk.listen(5) 
-conn,addr = sk.accept()  # 阻塞状态，被动等待客户端的连接
-def sendMessage(conn):
-      while True:
-          time.sleep(1)
-          send_data = input("输入发送内p容：")
-          conn.sendall(bytes(send_data, encoding="utf8"))
-def getMessage(conn):#接收信息
-      while True:
-          accept_data = str(conn.recv(1024),encoding="utf8")
-          logging.info(accept_data)
+from bcoding import bencode, bdecode
 
-#_thread.start_new_thread( sendMessage, (conn, ) )
-_thread.start_new_thread( getMessage, (conn, ) )   
-
-   
+ServerName = 'router.utorrent.com'
+ServerPort = 6881
+clientSocket =socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+msg={"t":"aa", "y":"q","q":"ping", "a":{"id":"abcdefghij0123456789"}}
+clientSocket.sendto(bencode(msg),(ServerName,ServerPort))
+modifiedmessage,serverAddress = clientSocket.recvfrom(2048)
+print (bdecode(modifiedmessage))
+clientSocket.close()
